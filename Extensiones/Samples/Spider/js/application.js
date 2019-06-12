e2ad18955ab256c6934a60d22a68cc827bb82661
difemaro <difemaro@hotmail.com>
2019-06-12 18:09:13 -0500

    Update application.js

--- Extensiones/Samples/Spider/js/application.js
+++ Extensiones/Samples/Spider/js/application.js
@@ -105,9 +105,7 @@
 		var datasetrow = {};
 		var dataset = [];
 		var marksData = {};
-		
-		console.log("Filled " + FilledBackground);
-		
+			
 		if (FilledBackground="Yes"){
 			var colors = ["rgba(166,206,227,0.2)", "rgba(178,223,138,0.2)", "rgba(251,154,153,0.2)", "rgba(253,191,111,0.2)",
 						  "rgba(202,178,214,0.2)", "rgba(255,255,153,0.2)", "rgba(31,120,180,0.2)", "rgba(51,160,44,0.2)"];	
@@ -117,6 +115,8 @@
 						  "rgba(202,178,214,1)", "rgba(255,255,153,1)", "rgba(31,120,180,0.2)", "rgba(51,160,44,1)"];
 			var FillChart = false;
 		}
+		
+		console.log("Filled " + FilledBackground + FillChart);
 					  
 		//Definir las series del Radial Chart
 		for (var i=0; i<worksheetData.length; i++) {
