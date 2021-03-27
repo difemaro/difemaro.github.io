94965aecf10cd205e41cd67874e32a3ad723af64
difemaro <difemaro@hotmail.com>
2021-03-26 22:10:39 -0500

    Cambio height

--- Extensiones/Samples/Spider/js/application.js
+++ Extensiones/Samples/Spider/js/application.js
@@ -104,7 +104,7 @@
 		radarChart.destroy();
 		console.log("destruir");
 	}
-	console.log("Act6");
+	console.log("Act7");
 	console.log("drawchart");
 	console.log(tableau.extensions.settings.get("worksheet"));
     worksheetName = tableau.extensions.settings.get("worksheet");
