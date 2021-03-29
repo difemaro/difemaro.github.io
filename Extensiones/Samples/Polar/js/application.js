e9d7f19db3b00dddffbe1f87c5dc0cffaa55fcc1
difemaro <difemaro@hotmail.com>
2021-03-29 09:24:40 -0500

    Polar

---
+++ Extensiones/Samples/Polar/js/application.js
@@ -0,0 +1,307 @@
+'use strict';
+
+(function () {
+  //let unregisterSettingsEventListener = null;	
+  let unregisterFilterEventListener = null;
+  let unregisterMarkSelectionEventListener = null;
+  let unregisterSettingsEventListener = null;
+  let worksheet = null;
+  let worksheetName = null;
+  let categoryColumnNumber = null;
+  //let categoryColumnNumberTo = null;
+  let valueColumnNumber = null;
+  let FilledBackground = null;
+  let Opacity = null;
+  let Legend = null;
+  let Axislabel = null;
+
+  $(document).ready(function () { 
+    tableau.extensions.initializeAsync({ 'configure':configure }).then(function () {
+      // Draw the chart when initialising the dashboard.
+		  console.log("initialize");
+		  //getSettings();
+		  //drawChartJS();
+		  if (tableau.extensions.settings.get("worksheet") != null){
+			  console.log("initialize2");
+			  getSettings();
+			  drawChartJS();
+		  }
+		  if (tableau.extensions.settings.get("configured") != 1) {
+				configure();
+	      }
+
+      // Set up the Settings Event Listener.
+      unregisterSettingsEventListener = tableau.extensions.settings.addEventListener(tableau.TableauEventType.SettingsChanged, (settingsEvent) => {
+        // On settings change.
+        console.log("SettingsChanged");
+		getSettings();
+        drawChartJS();
+      });
+    }, function () { console.log('Error while Initializing: ' + err.toString()); });
+  });
+
+  function getSettings() {
+    // Once the settings change populate global variables from the settings.
+    worksheetName = tableau.extensions.settings.get("worksheet");
+    categoryColumnNumber = tableau.extensions.settings.get("categoryColumnNumber");
+	//categoryColumnNumberTo = tableau.extensions.settings.get("categoryColumnNumberTo");
+    valueColumnNumber = tableau.extensions.settings.get("valueColumnNumber");
+	FilledBackground = tableau.extensions.settings.get("filled");
+	Opacity = tableau.extensions.settings.get("opacity");
+	Legend = tableau.extensions.settings.get("legend");
+	Axislabel = tableau.extensions.settings.get("axislabel");
+	
+    // If settings are changed we will unregister and re register the listener.
+    if (unregisterFilterEventListener != null) {
+      unregisterFilterEventListener();
+    }
+    // If settings are changed we will unregister and re register the listener.
+    if (unregisterMarkSelectionEventListener != null) {
+      unregisterMarkSelectionEventListener();
+    }
+
+    // Get worksheet
+	worksheet=tableau.extensions.dashboardContent.dashboard.worksheets.find(function (sheet) {
+		  return sheet.name===worksheetName;
+		});
+
+
+	
+    // Add listener
+	if (worksheet != undefined){
+		unregisterFilterEventListener = worksheet.addEventListener(tableau.TableauEventType.FilterChanged, (filterEvent) => {
+			console.log("ListenerFiltros");
+			drawChartJS();
+		});
+	}
+
+	if (worksheet != undefined){
+		unregisterMarkSelectionEventListener = worksheet.addEventListener(tableau.TableauEventType.MarkSelectionChanged, (filterEvent) => {
+			console.log("ListenerSeleccion");
+			drawChartJS();
+		});
+	}
+	
+	var body = document.getElementById("myBody");
+	body.addEventListener("resize", drawChartJS);
+
+
+	// Redraw Listener
+	if (document.addEventListener) {
+		console.log("Resize");
+		window.addEventListener('resize', drawChartJS);
+	}
+	else if (document.attachEvent) {
+		console.log("Onresize");
+		window.attachEvent('onresize', drawChartJS);
+	}
+	else {
+		console.log("Elseresize");
+		drawChartJS();
+	}
+  }
+
+  var polarChart;
+  
+  function drawChartJS() {
+	console.log("Act8");
+	console.log("drawchart");
+	console.log(tableau.extensions.settings.get("worksheet"));
+    worksheetName = tableau.extensions.settings.get("worksheet");
+    categoryColumnNumber = tableau.extensions.settings.get("categoryColumnNumber");
+	//categoryColumnNumberTo = tableau.extensions.settings.get("categoryColumnNumberTo");
+    valueColumnNumber = tableau.extensions.settings.get("valueColumnNumber");
+	FilledBackground = tableau.extensions.settings.get("filled");
+	Opacity = tableau.extensions.settings.get("opacity");
+	Legend = tableau.extensions.settings.get("legend");
+	Axislabel = tableau.extensions.settings.get("axislabel");
+	
+	worksheet=tableau.extensions.dashboardContent.dashboard.worksheets.find(function (sheet) {
+      return sheet.name===worksheetName;
+    });
+	
+    worksheet.getSummaryDataAsync().then(function (sumdata) {
+		var worksheetData = sumdata.data;
+		//var labels = [];
+		var label = [];
+		var datasetrow = {};
+		var dataset = [];
+		var marksData = {};
+		var colors = ["rgba(156,148,0," + Opacity + ")", "rgba(56,0,158," + Opacity + ")", "rgba(251,154,153," + Opacity + ")", "rgba(235,223,0," + Opacity + ")",
+					  "rgba(255,244,25," + Opacity + ")", "rgba(83,0,235," + Opacity + ")", "rgba(31,120,180,"  + Opacity + ")", "rgba(51,160,44,"   + Opacity + ")",
+					  "rgba(156,148,0," + Opacity + ")", "rgba(56,0,158," + Opacity + ")", "rgba(251,154,153," + Opacity + ")", "rgba(235,223,0," + Opacity + ")",
+					  "rgba(255,244,25," + Opacity + ")", "rgba(83,0,235," + Opacity + ")", "rgba(31,120,180,"  + Opacity + ")", "rgba(51,160,44,"   + Opacity + ")"];	
+		
+		if (FilledBackground == 1){
+			var FillChart = true;
+		}else{
+			var FillChart = false;
+		}
+		
+		if (Legend == 1){
+			var ShowLegend = true;
+		}else{
+			var ShowLegend = false;
+		}
+		
+		if (Axislabel == 1){
+			var ShowAxislabel = true;
+		}else{
+			var ShowAxislabel = false;
+		}
+		
+						  
+		//Definir las series del Radial Chart
+		for (var i=0; i<worksheetData.length; i++) {
+			if (label.length == 0){
+				label.push(worksheetData[i][categoryColumnNumber-1].formattedValue);
+			}
+			if (label.includes(worksheetData[i][categoryColumnNumber-1].formattedValue) == false){
+				label.push(worksheetData[i][categoryColumnNumber-1].formattedValue);
+			}
+		}
+		label.sort();
+		
+		//Definir las categorias del Radial Chart
+		//for (var i=0; i<worksheetData.length; i++) {
+		//	if (labels.length == 0){
+		//		labels.push(worksheetData[i][categoryColumnNumberTo-1].formattedValue);
+		//	}
+		//	if (labels.includes(worksheetData[i][categoryColumnNumberTo-1].formattedValue) == false ){
+		//		labels.push(worksheetData[i][categoryColumnNumberTo-1].formattedValue);
+		//	}
+		//}
+		//labels.sort();
+
+		//console.log("Label");
+		//console.log(label);
+		//console.log("Labels");
+		//console.log(labels);
+
+		//Definir los valores para cada serie
+		var dataserie = Array(label.length);
+				
+		//var k=0;
+		//console.log(worksheetData);		
+		
+		for (var i=0; i<label.length; i++) {
+			dataserie.fill(0);
+			for (var j=0; j<worksheetData.length; j++){
+				k=0;
+				while (worksheetData[j][categoryColumnNumber-1].formattedValue != labels[k]){
+					k=k+1;
+				}
+				dataserie[k] = dataserie[k] + worksheetData[j][valueColumnNumber-1].value;
+			}
+			//console.log("Dataserie");
+			//console.log(dataserie);
+			datasetrow = {label: label,
+						  backgroundColor: colors,
+						  data: Object.values(dataserie),
+						  fill: FillChart,
+						  borderColor:colors,
+						  borderWidth:1};
+			//console.log("Dataserow");
+			//console.log(datasetrow);
+			dataset.push(datasetrow);
+		}
+
+		//console.log("dataset");
+		//console.log(dataset);
+
+		marksData = {labels: labels,
+					 datasets: dataset
+					};
+
+		//console.log("MarksData");
+		//console.log(marksData);
+		
+		var options = {responsive: true,
+//					   animation: true,
+					   maintainAspectRatio: false,
+					   legend: {
+							display: ShowLegend
+					   },
+					   scale: {
+						   ticks: {
+							   min:0,
+							   display: ShowAxislabel
+						   }
+						   
+					   },
+					   tooltips: {
+									enabled: true,
+									callbacks: {
+										label: function(tooltipItem, data) {
+											return data.datasets[tooltipItem.datasetIndex].label + ' : ' + data.datasets[tooltipItem.datasetIndex].data[tooltipItem.index];
+										}
+									}
+								}					   
+					   };
+		
+		var width = 0.95*$("#myBody").parent().width();
+		var heigth = 0.95*$("#myBody").parent().height();
+		
+		console.log($("#myBody").parent());
+		
+		var minwh;
+		
+		if (width<heigth){
+			minwh=width;
+		}else{
+			minwh=heigth;
+		}
+		
+		console.log(minwh);
+		
+		var el = document.getElementById("myDiv");
+		el.style.height = minwh + "px";
+		el.style.width = minwh + "px";
+		
+		var bd = document.getElementById("myBody");
+		bd.style.height = minwh + "px";
+		bd.style.width = minwh + "px";
+		
+		var spinner = document.getElementById("spin");
+		spinner.style.display = "none";
+		
+		console.log(options);
+		
+		if (radarChart){
+			radarChart.config.data = marksData;
+			radarChart.options = options;
+			radarChart.update();
+		} else {
+		
+			radarChart = new Chart($("#myChart"), {
+			type: 'polarArea',
+			data: marksData,
+			options: options
+			});
+		}
+	})
+  }
+  
+  function configure() {
+	var spinner = document.getElementById("spin");
+	spinner.style.display ="inline-block";
+    const popupUrl=`${window.location.origin}/Extensiones/Samples/Spider/dialog.html`;
+    let defaultPayload="";
+    tableau.extensions.ui.displayDialogAsync(popupUrl, defaultPayload, { height:550, width:450 }).then((closePayload) => {
+		console.log("Exito");
+		drawChartJS();
+		var spinner = document.getElementById("spin");
+		spinner.style.display = "none";
+    }).catch((error) => {
+      switch (error.errorCode) {
+        case tableau.ErrorCodes.DialogClosedByUser:
+          console.log("Dialog was closed by user");
+		  var spinner = document.getElementById("spin");
+		  spinner.style.display = "none";
+          break;
+        default:
+          console.error(error.message);
+      }
+    });
+  }
+})();
---
+++ Extensiones/Samples/Polar/js/dialog.js
@@ -0,0 +1,116 @@
+'use strict';
+
+(function () {
+
+    $(document).ready(function () {
+        tableau.extensions.initializeDialogAsync().then(function (openPayload) {
+			buildDialog();
+        });
+    });
+
+    function buildDialog() {
+        let dashboard = tableau.extensions.dashboardContent.dashboard;
+        dashboard.worksheets.forEach(function (worksheet) {
+            $("#selectWorksheet").append("<option value='" + worksheet.name + "'>" + worksheet.name + "</option>");
+        });
+        var worksheetName = tableau.extensions.settings.get("worksheet");
+        if (worksheetName != undefined) {
+            $("#selectWorksheet").val(worksheetName);
+            columnsUpdate();
+        }
+
+        $('#selectWorksheet').on('change', '', function (e) {
+            columnsUpdate();
+        });
+        $('#cancel').click(closeDialog);
+        $('#save').click(saveButton);
+        //$('.select').select2();
+    }
+
+    function columnsUpdate() {
+
+        var worksheets = tableau.extensions.dashboardContent.dashboard.worksheets;
+        var worksheetName = $("#selectWorksheet").val();
+
+        var worksheet = worksheets.find(function (sheet) {
+            return sheet.name === worksheetName;
+        });       
+		
+		worksheet.getSummaryDataAsync({ maxRows: 1 }).then(function (sumdata) {
+            var worksheetColumns = sumdata.columns;
+			console.log(worksheetColumns);
+            $("#selectCategoryFrom").text("");
+            $("#selectValue").text("");
+			$("#selectFill").text("");
+			$("#selectLegend").text("");
+			$("#selectAxisLabels").text("");
+			$("#selectOpacity").text("");			
+			
+            var counter = 1;
+            worksheetColumns.forEach(function (current_value) {
+				if (current_value.dataType != "integer" && current_value.dataType != "float"){
+					$("#selectCategoryFrom").append("<option value='" + counter + "'>" + current_value.fieldName + "</option>");
+				}else{
+				$("#selectValue").append("<option value='" + counter + "'>"+current_value.fieldName+"</option>");
+				}
+                counter++;
+            });
+					
+			//Llenar el box de rellenar o no			
+			$("#selectFill").append("<option value='" + 1 + "'>"+"Yes"+"</option>");
+			$("#selectFill").append("<option value='" + 2 + "'>"+"No"+"</option>");
+
+			//Llenar el box de Legend
+			$("#selectLegend").append("<option value='" + 1 + "'>"+"Yes"+"</option>");
+			$("#selectLegend").append("<option value='" + 2 + "'>"+"No"+"</option>");
+
+			//Llenar el box de Legend
+			$("#selectAxisLabels").append("<option value='" + 1 + "'>"+"Yes"+"</option>");
+			$("#selectAxisLabels").append("<option value='" + 2 + "'>"+"No"+"</option>");
+			
+			//Llenar el box de opacidad
+			$("#selectOpacity").append("<option value='" + 0.1 + "'>"+"10"+"</option>");
+			$("#selectOpacity").append("<option value='" + 0.2 + "'>"+"20"+"</option>");
+			$("#selectOpacity").append("<option value='" + 0.3 + "'>"+"30"+"</option>");
+			$("#selectOpacity").append("<option value='" + 0.4 + "'>"+"40"+"</option>");
+			$("#selectOpacity").append("<option value='" + 0.5 + "'>"+"50"+"</option>");
+			$("#selectOpacity").append("<option value='" + 0.6 + "'>"+"60"+"</option>");
+			$("#selectOpacity").append("<option value='" + 0.7 + "'>"+"70"+"</option>");
+			$("#selectOpacity").append("<option value='" + 0.8 + "'>"+"80"+"</option>");
+			$("#selectOpacity").append("<option value='" + 0.9 + "'>"+"90"+"</option>");
+			$("#selectOpacity").append("<option value='" + 1 + "'>"+"100"+"</option>");
+			
+			//Llenar las casillas si hay una configuración cargada
+            $("#selectCategoryFrom").val(tableau.extensions.settings.get("categoryColumnNumber"));
+			$("#selectCategoryTo").val(tableau.extensions.settings.get("categoryColumnNumberTo"));
+            $("#selectValue").val(tableau.extensions.settings.get("valueColumnNumber"));
+			$("#selectFill").val(tableau.extensions.settings.get("filled"));
+			$("#selectLegend").val(tableau.extensions.settings.get("legend"));
+			$("#selectAxisLabels").val(tableau.extensions.settings.get("axislabel"));			
+			$("#selectOpacity").val(tableau.extensions.settings.get("opacity"));
+        });
+    }
+
+    function reloadSettings() {
+        
+    }
+
+    function closeDialog() {
+        tableau.extensions.ui.closeDialog("10");
+    }
+
+    function saveButton() {
+		console.log($("#selectWorksheet").val()); 
+        tableau.extensions.settings.set("worksheet", $("#selectWorksheet").val());
+        tableau.extensions.settings.set("categoryColumnNumber", $("#selectCategoryFrom").val());
+        tableau.extensions.settings.set("valueColumnNumber", $("#selectValue").val());
+		tableau.extensions.settings.set("filled", $("#selectFill").val());
+		tableau.extensions.settings.set("legend", $("#selectLegend").val());
+		tableau.extensions.settings.set("axislabel", $("#selectAxisLabels").val());		
+		tableau.extensions.settings.set("opacity", $("#selectOpacity").val());
+		tableau.extensions.settings.set("configured", 1);
+		tableau.extensions.settings.saveAsync().then((currentSettings) => {
+			tableau.extensions.ui.closeDialog("10");
+        });
+    }
+})();
